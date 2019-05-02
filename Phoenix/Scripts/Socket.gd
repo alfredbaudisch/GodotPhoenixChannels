94a89b521b8eb19ab347c81aad7e7d0ec5bfba32
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2019-05-02 18:24:21 +0200

    Socket reconnect timers and mechanism

--- Phoenix/Scripts/Socket.gd
+++ Phoenix/Scripts/Socket.gd
@@ -24,10 +24,17 @@ var _is_https := false
 var _endpoint_url := ""
 var _last_status := -1
 var _connected_at := -1
+var _last_connected_at := -1
 var _join_ref := "0"
 var _join_ref_pos := 0
+var _requested_disconnect := false
+
 var _last_heartbeat_at := 0
 var _pending_heartbeat_ref := 0
+
+var _last_reconnect_try_at := -1
+var _should_reconnect := false
+var _reconnect_after_pos := 0
 
 export var is_connected = false setget ,get_is_connected
 
@@ -56,8 +63,8 @@ func _init(endpoint, opts = {}):
 
 func _ready():
 	_socket.connect("connection_established", self, "_on_socket_connected")
-	_socket.connect("connection_error", self, "_on_socket_error", ["error"])
-	_socket.connect("connection_closed", self, "_on_socket_error", ["closed"])
+	_socket.connect("connection_error", self, "_on_socket_error")
+	_socket.connect("connection_closed", self, "_on_socket_closed")
 	_socket.connect("data_received", self, "_on_socket_data_received")
 	
 	set_process(true)
@@ -69,12 +76,11 @@ func _process(delta):
 	var status = _socket.get_connection_status()
 
 	if status != _last_status:
-		_last_status = status
-				
-		if status == WebSocketClient.CONNECTION_CONNECTED:
-			is_connected = true
-		elif status == WebSocketClient.CONNECTION_DISCONNECTED:
+		_last_status = status			
+	
+		if status == WebSocketClient.CONNECTION_DISCONNECTED:
 			is_connected = false
+			_last_connected_at = _connected_at
 			_connected_at = -1
 			
 	if status == WebSocketClient.CONNECTION_CONNECTED:
@@ -84,6 +90,7 @@ func _process(delta):
 			_heartbeat(current_ticks)
 			
 	if status == WebSocketClient.CONNECTION_DISCONNECTED: 
+		_retry_reconnect(OS.get_ticks_msec())
 		return
 
 	_socket.poll()
@@ -96,13 +103,14 @@ func connect_socket():
 	if is_connected:
 		return
 	
-	_socket.verify_ssl = _is_https
+	_socket.verify_ssl = false
 	_socket.connect_to_url(_endpoint_url)
 	
 func disconnect_socket():
 	if not is_connected:
 		return
 	
+	_requested_disconnect = true
 	_socket.disconnect_from_host()	
 
 func get_is_connected() -> bool:
@@ -111,6 +119,29 @@ func get_is_connected() -> bool:
 #
 # Implementation 
 #
+
+func _reset_reconnection():
+	_last_reconnect_try_at = -1
+	_should_reconnect = false
+	_reconnect_after_pos = 0
+
+func _retry_reconnect(current_time):
+	if _should_reconnect:
+		# Just started the reconnection timer, set time as now, so the
+		# first _reconnect_after_pos amount will be subtracted from now
+		if _last_reconnect_try_at == -1:
+			_last_reconnect_try_at = current_time
+		else:
+			var reconnect_after = _settings.reconnect_after[_reconnect_after_pos]
+							
+			if current_time - _last_reconnect_try_at >= reconnect_after:
+				_last_reconnect_try_at = current_time
+				
+				# Move to the next reconnect time (or keep the last one)
+				if _reconnect_after_pos < reconnect_after - 1 and _reconnect_after_pos < _settings.reconnect_after.size() - 1: 
+					_reconnect_after_pos += 1
+					
+				connect_socket()
 
 func _make_ref() -> String:
 	_ref = _ref + 1
@@ -161,7 +192,6 @@ func _push_message(message):
 	if _can_send_message(message.event):
 		print(message)
 		_socket.get_peer(1).put_packet(to_json(message).to_utf8())		
-		
 
 #
 # Listeners
@@ -169,16 +199,32 @@ func _push_message(message):
 
 func _on_socket_connected(protocol):
 	_socket.get_peer(1).set_write_mode(WRITE_MODE)
-	_connected_at = OS.get_ticks_msec()
+	
+	_connected_at = OS.get_ticks_msec()	
+	_last_heartbeat_at = 0
+	_requested_disconnect = false
+	_reset_reconnection()
+	
+	is_connected = true	
 	emit_signal("on_open", {})
 	
-func _on_socket_error(arg, error = null):
-	print("_on_socket_error: ", error)
+func _on_socket_error(reason = null):
+	if not is_connected or (_connected_at == -1 and _last_connected_at != -1):
+		_should_reconnect = true
+
+	print("_on_socket_closed: ", reason)
+	emit_signal("on_error", reason)
+		
+func _on_socket_closed(clean):
+	if not _requested_disconnect:
+		_should_reconnect = true
 	
-	if error == "closed":
-		emit_signal("on_close", {})
-	else:
-		emit_signal("on_error", {})		
+	var payload = {
+		requested = _requested_disconnect,
+		will_reconnect = not _requested_disconnect
+	}
+	print("_on_socket_closed: ", payload)
+	emit_signal("on_close", payload)
 	
 func _on_socket_data_received(pid := 1):
     print("_on_socket_data_received")
