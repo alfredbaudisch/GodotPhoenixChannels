f2e1f12070a0429e07cd81b0bc5af09c89567b30
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2019-05-09 19:12:53 +0200

    Implemented Presence.syncDiff and list from the JS library

--- Phoenix/Scripts/Channel.gd
+++ Phoenix/Scripts/Channel.gd
@@ -41,57 +41,6 @@ var _rejoin_timer : Timer
 var _should_rejoin_until_connected := false
 var _rejoin_pos := -1
 
-class Presence:
-	var _state := {}
-	
-	func sync_state(new_state : Dictionary):		
-		var joins := {}
-		var leaves := {}
-		
-		var keys := _state.keys()
-		for key in keys:
-			if not new_state.has(key):
-				leaves[key] = _state[key]
-		
-		var new_state_keys := new_state.keys()		
-		for key in new_state_keys:			
-			var new_presence = new_state[key]
-			
-			if _state.has(key):
-				var current_presence = _state[key]
-				var new_refs := PhoenixUtils.map(funcref(self, "_get_phx_ref"), new_presence)
-				var curr_refs := PhoenixUtils.map(funcref(self, "_get_phx_ref"), current_presence)
-				
-				var joined_metas := _find_metas_from_refs(new_presence.metas, curr_refs)
-				var left_metas := _find_metas_from_refs(current_presence.metas, new_refs)
-						
-				if joined_metas.size() > 0:
-					joins[key] = new_presence
-					joins[key].metas = joined_metas
-					
-				if left_metas.size() > 0:
-					leaves[key] = current_presence.duplicate(true)
-					leaves[key].metas = left_metas					
-			
-			else:
-				joins[key] = new_presence
-		
-	func sync_diff(diff):
-		pass	
-		
-	func _get_phx_ref(presence : Dictionary) -> String:
-		return presence.phx_ref			
-	
-	func _find_metas_from_refs(metas : Array, refs : Array) -> Array:	
-		var final_metas := []
-			
-		for meta in metas:
-			var pos := refs.find(meta.phx_ref)
-			if pos != -1:
-				final_metas.append(meta)
-		
-		return final_metas
-
 func _init(socket, topic, params : Dictionary = {}):
 	assert(topic != TOPIC_PHOENIX)
 	_socket = socket
@@ -281,3 +230,103 @@ func _on_Timer_timeout():
 	if _should_rejoin_until_connected:
 		if not _joined_once:
 			_rejoin()
+		
+#
+# Presence Inner Class
+#
+
+class Presence:
+	var _state := {}
+	
+	func sync_state(new_state : Dictionary):		
+		var joins := {}
+		var leaves := {}
+		
+		var keys := _state.keys()
+		for key in keys:
+			if not new_state.has(key):
+				leaves[key] = _state[key]
+		
+		var new_state_keys := new_state.keys()		
+		for key in new_state_keys:			
+			var new_presence = new_state[key]
+			
+			if _state.has(key):
+				var current_presence = _state[key]
+				var new_refs := PhoenixUtils.map(funcref(self, "_get_phx_ref"), new_presence)
+				var curr_refs := PhoenixUtils.map(funcref(self, "_get_phx_ref"), current_presence)
+				
+				var joined_metas := _find_metas_from_refs(new_presence.metas, curr_refs)
+				var left_metas := _find_metas_from_refs(current_presence.metas, new_refs)
+						
+				if joined_metas.size() > 0:
+					joins[key] = new_presence
+					joins[key].metas = joined_metas
+					
+				if left_metas.size() > 0:
+					leaves[key] = current_presence.duplicate(true)
+					leaves[key].metas = left_metas					
+			
+			else:
+				joins[key] = new_presence
+		
+		return sync_diff({
+			joins = joins,
+			leaves = leaves
+		})
+		
+	func sync_diff(diff):
+		var joins = diff.joins
+		var leaves = diff.leaves
+		
+		var keys = joins.keys()
+		for key in keys:
+			var new_presence = joins[key]			
+			var current_presence = _state[key] if _state.has(key) else null
+			_state[key] = new_presence
+			
+			if current_presence:
+				for meta in current_presence.metas:
+					_state[key].metas.push_front(meta)
+			
+			#on_join(key, current_presence, new_presence)
+			
+		keys = leaves.keys()
+		for key in keys:
+			var left_presence = leaves[key]
+			
+			if _state.has(key):
+				var current_presence = _state[key]				
+				var refs_to_remove = PhoenixUtils.map(funcref(self, "_get_phx_ref"), left_presence.metas)				
+				current_presence.metas = _find_metas_from_refs(current_presence.metas, refs_to_remove)
+				
+				#on_leave(key, current_presence, left_presence)
+				
+				if current_presence.metas.size() == 0:
+					_state.erase(key)
+		
+		return _state
+		
+	func list(chooser : FuncRef = null):
+		if chooser:
+			var sorted := []		
+			for key in _state.keys():
+				sorted.append(chooser.call_func(key, _state[key]))
+							
+			return sorted
+		
+		else:
+			return _state.values()
+
+	func _get_phx_ref(presence : Dictionary) -> String:
+		return presence.phx_ref			
+	
+	func _find_metas_from_refs(metas : Array, refs : Array) -> Array:	
+		var final_metas := []
+			
+		for meta in metas:
+			var pos := refs.find(meta.phx_ref)
+			if pos == -1:
+				final_metas.append(meta)
+		
+		return final_metas
